89f470b1a3db719c38681f570668ab72a315dff5
Clayton Blythe <claytonb@zg03162.zillow.local>
2019-05-26 21:56:50 -0700

    refactor code for python3 and OOP

--- code/subredditTopImagesDownloader.py
+++ code/subredditTopImagesDownloader.py
@@ -1,121 +1,77 @@
-
-# coding: utf-8
-
-# In[10]:
-
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import requests
 from tqdm import tqdm
 import os
 import argparse
-# Get a copy of the default headers that requests would use
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36'}
 
 
-# In[11]:
+class SubbredditTopPostsGenerator:
+    def __init__(self, base_url, subreddit):
+        self.image_urls = []
+        self.baseUrl = base_url
+        self.session = requests.session()
+        self.extensions = ['png', 'jpg', 'jpeg', 'ifv2']
+        self.domains = ["i.imgur.com", "i.redd.it", "cdnb.artstation.com"]
+        self.subreddit = subreddit
+        self.first_iter = True
+        self.next_url = base_url
+
+    def __next__(self):
+        r = self.session.get(self.next_url, headers=headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        self.image_urls += [tag['data-url'] for domain in self.domains for tag in
+                      soup.find_all('div', {"data-domain": domain}) if
+                      tag['data-url'][-3:] in self.extensions]
+        self.next_url = soup.find_all('span', {"class": "next-button"})[0].find_all('a')[0]['href']
+
+    def __iter__(self):
+        return self
+
+    def save_urls(self):
+        if not os.path.exists('../figures/' + subreddit + '/' + timeframe):
+            os.makedirs('../figures/' + subreddit + '/' + timeframe)
+
+        for idx in tqdm(range(len(self.image_urls))):
+            this_image_url = self.image_urls[idx]
+
+            if not os.path.exists(
+                    '../figures/' + subreddit + '/' + timeframe + '/' + this_image_url[7:].replace('/', '_').strip('_')):
+                r = self.session.get(this_image_url, headers=headers, stream=True)
+                with open('../figures/' + subreddit + '/' + timeframe + '/' + this_image_url[7:].replace('/', '_').strip('_'),
+                          'wb') as f:
+                    for chunk in r.iter_content(chunk_size=1024):
+                        if chunk:  # filter out keep-alive new chunks
+                            f.write(chunk)
+            else:
+                continue
+
 
 def get_args():
-    '''This function parses and return arguments passed in'''
-    # Assign description to the help doc
+    """This function parses and return arguments passed in"""
     parser = argparse.ArgumentParser(
         description='Script saves images from subreddit')
-    # Add arguments
     parser.add_argument(
         '-s', '--subreddit', type=str, help='Subreddit name', default='EarthPorn')
     parser.add_argument(
-        '-n', '--numPages', type=int, help='Number of pages to save from', default=1)
+        '-n', '--num_pages', type=int, help='Number of pages to save from', default=1)
     parser.add_argument(
-        '-t', '--timeFrame', type=str, help='Time frame to save from', default='all')
-    # Array for all arguments passed to script
+        '-t', '--timeframe', type=str, help='Time frame to save from', default='all')
     args = parser.parse_args()
-    # Assign args to variables
     subreddit = args.subreddit
-    numPages = args.numPages
-    timeFrame = args.timeFrame
-    # Return all variable values
-    return subreddit, numPages, timeFrame
-
-# Run get_args()
-# get_args()
-
-# Match return values from get_arguments()
-# and assign to their respective variables
-subreddit, numPages, timeFrame = get_args()
+    num_pages = args.num_pages
+    timeframe = args.timeframe
+    return subreddit, num_pages, timeframe
 
 
-# In[12]:
+if __name__ == "__main__":
+    subreddit, num_pages, timeframe = get_args()
+    base_url = f"https://old.reddit.com/r/{subreddit}/top/?sort=top&t={timeframe}"
+    my_iterator = iter(SubbredditTopPostsGenerator(base_url, subreddit))
+    for _ in range(num_pages):
+        next(my_iterator)
 
-url = "https://www.reddit.com/r/{}/top/?sort=top&t={}".format(subreddit,timeFrame)
-s = requests.session()
-r = s.get(url, headers = headers)
-soup = bs(r.text, 'lxml')
-extensions = ['png', 'jpg', 'jpeg','ifv2']
-
-
-# In[8]:
-
-class subredditTopPostsGenerator:
-    def __init__(self, url):
-        self.urls = []
-        self.baseUrl = url
-        r = s.get(url, headers = headers)
-        soup = bs(r.text, 'lxml')
-        self.urls += [tag['data-url'] for tag in soup.find_all('div', {"data-domain": "i.imgur.com"}) if tag['data-url'][-3:] in extensions]
-        self.urls += [tag['data-url'] for tag in soup.find_all('div', {"data-domain":"i.redd.it"})if tag['data-url'][-3:] in extensions]
-        self.urls += [tag['data-url'] for tag in soup.find_all('div', {"data-domain":"cdnb.artstation.com"})if tag['data-url'][-3:] in extensions]
-        if numPages > 1:
-            self.nextUrl = soup.find_all('span', {"class":"next-button"})[0].find_all('a')[0]['href']
-        else:
-            self.nextUrl='None'
-    def __next__(self):
-        temp_r = s.get(self.nextUrl, headers=headers)
-        soup = bs(temp_r.text, 'lxml')
-        self.urls += [tag['data-url'] for tag in soup.find_all('div', {"data-domain": "i.imgur.com"}) if tag['data-url'][-3:] in extensions]
-        self.urls += [tag['data-url'] for tag in soup.find_all('div', {"data-domain":"i.redd.it"})if tag['data-url'][-3:] in extensions]
-        self.urls += [tag['data-url'] for tag in soup.find_all('div', {"data-domain":"cdnb.artstation.com"})if tag['data-url'][-3:] in extensions]
-        self.nextUrl = soup.find_all('span', {"class":"next-button"})[0].find_all('a')[0]['href']
-
-    def __iter__(self):
-        return(self)
-
-
-# In[9]:
-
-myurl =  "https://www.reddit.com/r/{}/top/?sort=top&t={}".format(subreddit,timeFrame)
-myiterator = iter(subredditTopPostsGenerator(myurl))
-
-
-# In[4]:
-
-def saveUrls(subreddit, urls):
-    print("Downloading wallpapers from " "r/" + subreddit)
-    
-    if not os.path.exists('../figures/'+subreddit +'/' + timeFrame):
-        os.makedirs('../figures/'+subreddit + '/' + timeFrame)
-
-    for i in tqdm(range(len(urls))):
-        #print("Downloading..."+subreddit+'/' + tempUrl[7:].replace('/', '_').strip('_'))
-        tempUrl = urls[i]
-        r = s.get(tempUrl, headers=headers, stream=True)
-        
-        if not os.path.exists('../figures/' + subreddit + '/' + timeFrame + '/' + tempUrl[7:].replace('/', '_').strip('_')):
-            with open('../figures/' + subreddit + '/' + timeFrame + '/' + tempUrl[7:].replace('/', '_').strip('_') , 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024): 
-                    if chunk: # filter out keep-alive new chunks
-                        f.write(chunk)
-        else:
-            continue
-
-
-# In[5]:
-
-if numPages > 1:
-    for i in range(numPages - 1):
-        next(myiterator)
-#print(myiterator.urls)
-
-
-# In[6]:
-
-saveUrls(subreddit, myiterator.urls)
-
+    my_iterator.save_urls()
